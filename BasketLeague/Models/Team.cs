c3d7be77911d8220fe0f9f03e0a98f20d9862243
IanLiceranzu <ianliceranzu@gmail.com>
2020-01-20 20:24:13 +0100

    Agregar archivos de proyecto.

---
+++ BasketLeague/Models/Team.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BasketLeague.Models
+{
+    class Team
+    {
+        public string Nombre { get; set; }
+        public int Ataque { get; set; }
+        public int Defensa { get; set; }
+        public int Rebote { get; set; }
+        public int Tiro { get; set; }
+
+        public int Atacar(Team rival)
+        {
+            Random rnd = new Random();
+            return 80 + (rnd.Next(5, 10) * Ataque + rnd.Next(5, 10) * Tiro) - (rnd.Next(1, 5) * rival.Defensa + rnd.Next(1, 5) * rival.Rebote);
+        }
+
+        public int Defender(Team rival)
+        {
+            Random rnd = new Random();
+            return 30 + (rnd.Next(5, 10) * Defensa + rnd.Next(5, 10) * Rebote) - (rnd.Next(1, 5) * rival.Ataque + rnd.Next(1, 5) * rival.Tiro);
+        }
+
+        public int Resultado(int defender, int atacar)
+        {
+            Random rnd = new Random();
+            return rnd.Next(defender, atacar);
+        }
+
+        public override string ToString()
+        {
+            return Nombre;
+        }
+    }
+}
---
+++ BasketLeague/Program.cs
@@ -0,0 +1,77 @@
+﻿using BasketLeague.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BasketLeague
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a la competición");
+
+            Console.WriteLine("Introduce la ruta del archivo con los equipos");
+
+            string path = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                path = @"C:\Users\PcCom\Desktop\Prueba.txt";
+            }
+
+            List<Team> teams = new List<Team>();
+
+            try
+            {
+                StreamReader sr = new StreamReader(@path);
+
+                var line = sr.ReadLine();
+                while (line != null)
+                {
+                    Team team = new Team()
+                    {
+                        Nombre = line.Split(' ')[0],
+                        Ataque = int.Parse(line.Split(' ')[1]),
+                        Defensa = int.Parse(line.Split(' ')[2]),
+                        Rebote = int.Parse(line.Split(' ')[3]),
+                        Tiro = int.Parse(line.Split(' ')[4])
+                    };
+
+                    line = sr.ReadLine();
+
+                    teams.Add(team);
+                }
+                sr.Close();
+
+                Console.WriteLine("Equipos disponibles:");
+
+                for (int i = 0; i < teams.Count; i++)
+                {
+                    Console.WriteLine(i + " - " + teams[i].ToString());
+                }
+
+                Console.WriteLine("----------");
+
+                Console.WriteLine("Equipo que juega en casa:");
+                Team home = teams[int.Parse(Console.ReadLine())];
+
+                Console.WriteLine("Equipo contrario:");
+                Team rival = teams[int.Parse(Console.ReadLine())];
+
+                int ha = home.Atacar(rival);
+                int hd = home.Defender(rival);
+                int ra = rival.Atacar(home);
+                int rd = rival.Defender(home);
+
+                Console.WriteLine(string.Format("Home: {0} ({1} - {2})", home.Resultado(hd, ha), hd, ha));
+                Console.WriteLine(string.Format("Rival: {0} ({1} - {2})", rival.Resultado(rd, ra), rd, ra));
+            }
+            finally
+            {
+                Console.WriteLine("Programa finalizado");
+                Console.ReadLine();
+            }
+        }
+    }
+}
