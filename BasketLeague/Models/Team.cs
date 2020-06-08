98a42a5f6560d9236c3f2a2af517ef0e9adc10aa
Ian <43787552+Ian-Inizias@users.noreply.github.com>
2020-06-08 09:06:44 +0200

    Comentar + Refactorizar

--- BasketLeague/Models/Team.cs
+++ BasketLeague/Models/Team.cs
@@ -4,23 +4,59 @@ namespace BasketLeague.Models
 {
     class Team
     {
+        /// <summary>
+        /// Nombre completo del equipo
+        /// </summary>
         public string NombreCompleto { get; set; }
+        /// <summary>
+        /// Abreviación del nombre del equipo
+        /// </summary>
         public string Nombre { get; set; }
+        /// <summary>
+        /// Puntuación en ataque del equipo
+        /// </summary>
         public int Ataque { get; set; }
+        /// <summary>
+        /// Puntuación en defensa del equipo
+        /// </summary>
         public int Defensa { get; set; }
+        /// <summary>
+        /// Puntuación en tiro del equipo
+        /// </summary>
         public int Tiro { get; set; }
+        /// <summary>
+        /// Puntuación en rebote del equipo
+        /// </summary>
         public int Rebote { get; set; }
 
+        /// <summary>
+        /// El equipo en cuestión ataca a un rival
+        /// </summary>
+        /// <param name="rival">Equipo rival</param>
+        /// <param name="rnd">Random number generator</param>
+        /// <returns>Puntuación máxima a la que puede aspirar el quipo</returns>
         public int Atacar(Team rival, Random rnd)
         {
             return 80 + (rnd.Next(5, 10) * Ataque + rnd.Next(5, 10) * Tiro) - (rnd.Next(1, 5) * rival.Defensa + rnd.Next(1, 5) * rival.Rebote);
         }
 
+        /// <summary>
+        /// El equipo en cuestión se defiende del rival
+        /// </summary>
+        /// <param name="rival">Equipo rival</param>
+        /// <param name="rnd">Random number generator</param>
+        /// <returns>Puntuación mínima a la que puede aspirar el quipo</returns>
         public int Defender(Team rival, Random rnd)
         {
             return 50 + (rnd.Next(5, 10) * Defensa + rnd.Next(5, 10) * Rebote) - (rnd.Next(1, 5) * rival.Ataque + rnd.Next(1, 5) * rival.Tiro);
         }
 
+        /// <summary>
+        /// Sabiendo el máximo y el mínimo posibles para el equipo, devuelve un resultado
+        /// </summary>
+        /// <param name="defender">Mínimo al que puede llegar el equipo debido a su defensa</param>
+        /// <param name="atacar">Máximo al que puede llegar el equipo debido a su ataque</param>
+        /// <returns>Puntos finales del equipo</returns>
         public int Resultado(int defender, int atacar)
         {
             Random rnd = new Random();
--- BasketLeague/Program.cs
+++ BasketLeague/Program.cs
@@ -11,32 +11,9 @@ namespace BasketLeague
         {
             Console.WriteLine("Bienvenido a la competición");
 
-            string path = @"Data\Teams.txt";
-
-            List<Team> teams = new List<Team>();
-
             try
             {
-                StreamReader sr = new StreamReader(@path);
-
-                var line = sr.ReadLine();
-                while (line != null)
-                {
-                    Team team = new Team()
-                    {
-                        Nombre = line.Split(' ')[0],
-                        Ataque = int.Parse(line.Split(' ')[1]),
-                        Defensa = int.Parse(line.Split(' ')[2]),
-                        Tiro = int.Parse(line.Split(' ')[3]),
-                        Rebote = int.Parse(line.Split(' ')[4]),
-                        NombreCompleto = line.Split(' ')[5].Replace('_', ' ')
-                    };
-
-                    line = sr.ReadLine();
-
-                    teams.Add(team);
-                }
-                sr.Close();
+                List<Team> teams = CargarEquipos();
 
                 Console.WriteLine("Equipos disponibles:");
 
@@ -62,7 +39,7 @@ namespace BasketLeague
                 int rd = rival.Defender(home, rnd);
 
                 Console.WriteLine("----------");
-                if (ha < hd && ra < rd)
+                if (ha < hd && ra < rd) // En caso de dos equipos muy defensivos, el resultado se divide entre 2
                 {
                     Console.WriteLine(string.Format("{0}: {1}", home.NombreCompleto, home.Resultado(hd, ha) / 2));
                     Console.WriteLine(string.Format("{0}: {1}", rival.NombreCompleto, rival.Resultado(rd, ra) / 2));
@@ -79,6 +56,40 @@ namespace BasketLeague
                 Console.WriteLine("Programa finalizado");
                 Console.ReadLine();
             }
+        }
+
+        /// <summary>
+        /// Carga los equipos del archivo Teams.txt
+        /// </summary>
+        /// <returns>Lista de los equipos cargados</returns>
+        static List<Team> CargarEquipos()
+        {
+            string path = @"Data\Teams.txt";
+
+            List<Team> teams = new List<Team>();
+
+            StreamReader sr = new StreamReader(@path);
+
+            var line = sr.ReadLine();
+            while (line != null)
+            {
+                Team team = new Team()
+                {
+                    Nombre = line.Split(' ')[0],
+                    Ataque = int.Parse(line.Split(' ')[1]),
+                    Defensa = int.Parse(line.Split(' ')[2]),
+                    Tiro = int.Parse(line.Split(' ')[3]),
+                    Rebote = int.Parse(line.Split(' ')[4]),
+                    NombreCompleto = line.Split(' ')[5].Replace('_', ' ')
+                };
+
+                line = sr.ReadLine();
+
+                teams.Add(team);
+            }
+            sr.Close();
+
+            return teams;
         }
     }
 }
